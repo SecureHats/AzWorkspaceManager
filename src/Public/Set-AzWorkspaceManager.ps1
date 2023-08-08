b1545b59b041fe34ba95e575757da0ddf5ee90be
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-08-08 22:30:54 +0200

    Update Set-AzWorkspaceManager.ps1

--- src/Public/Set-AzWorkspaceManager.ps1
+++ src/Public/Set-AzWorkspaceManager.ps1
@@ -1,31 +1,47 @@
 function Set-AzWorkspaceManager {
     <#
+      .NAME
+          Set-AzWorkspaceManager
       .SYNOPSIS
-      Set Microsoft Sentinel Workspace Manager
+          Creates a Workspace Manager Configuration
       .DESCRIPTION
-      With this function you can set the Microsoft Sentinel Workspace Manager
+          The Set-AzWorkspaceManager cmdlet creates a Workspace Manager Configuration that is required to use workspace manager feature.
+          You can create a workspace manager configuration by using just a workspacename. The minimum requirement to to enable the 
+          workspace manager is that Microsoft Sentinel is enabled on the Log Analytics workspace.
+          Only one workspace manager configuration can be added per Microsoft Sentinel instance.
       .PARAMETER Name
-      Name of the log analytics workspace
+          Name of the log analytics workspace
       .PARAMETER ResourceGroupName
-      Name of the ResouceGroup where the log analytics workspace is located
+          Name of the ResouceGroup where the log analytics workspace is located
       .PARAMETER Mode
-      Status of the Workspace Manager (Enabled or Disabled)
-      .EXAMPLE
-      Set-AzWorkspaceManager -Name 'workspaceName' -ResourceGroupName 'resourceGroupName' -Mode 'Enabled'
+          Status of the Workspace Manager (Enabled or Disabled)
+      .EXAMPLE: Create Workspace Manager on a Sentinel workspace 
+          Set-AzWorkspaceManager -Name 'myWorkspace'
+
+          This command creates / enables the workspace manager on the Sentinel workspace 'myWorkspace'
+      .EXAMPLE: Disable the Workspace Manager on a Sentinel Workspace 
+          Set-AzWorkspaceManager -Name 'myworkspace' -Mode 'Disabled'
+
+          This command sets the workspace manager to disabled.
+      .EXAMPLE: Enable a Workspace Manager in the specified resourcegroup
+          Set-AzWorkspaceManager -Name 'myWorkspace' -ResourceGroupName 'myRG'
+
+          This command enables the workspace manager for the workspace 'myWorkspace' in resource group 'myRg'
+          Specifying the resource group is only needed if multiple workspaces with the same name are available in the subscription.
     #>
     [cmdletbinding()]
     param (
         [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
         [ValidatePattern('^[A-Za-z0-9][A-Za-z0-9-]+[A-Za-z0-9]$', ErrorMessage="It does not match expected pattern '{1}'")]
-        [string]$WorkspaceName,
+        [string]$Name,
 
         [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
         [string]$ResourceGroupName,
 
         [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
         [ValidateSet("Enabled", "Disabled")]
-        [string]$Mode
+        [string]$Mode = Enabled
     )
 
     begin {
@@ -34,10 +50,10 @@ function Set-AzWorkspaceManager {
 
     process {
         if ($ResourceGroupName) {
-            Get-LogAnalyticsWorkspace -Name $WorkspaceName -ResourceGroupName $ResourceGroupName
+            Get-LogAnalyticsWorkspace -Name $Name -ResourceGroupName $ResourceGroupName
         }
         else {
-            Get-LogAnalyticsWorkspace -Name $WorkspaceName
+            Get-LogAnalyticsWorkspace -Name $Name
         } 
         
         $payload = @{
@@ -49,8 +65,8 @@ function Set-AzWorkspaceManager {
         Write-Debug $payload
         try {
             if ($SessionVariables.workspace) {
-                Write-Verbose "Configuring Microsoft Sentinel Workspace Manager Configuration for workspace [$WorkspaceName]"
-                $uri = "$($SessionVariables.workspace)/providers/Microsoft.SecurityInsights/workspaceManagerConfigurations/$($WorkspaceName)?api-version=$($SessionVariables.apiVersion)"
+                Write-Verbose "Configuring Microsoft Sentinel Workspace Manager Configuration for workspace [$Name]"
+                $uri = "$($SessionVariables.workspace)/providers/Microsoft.SecurityInsights/workspaceManagerConfigurations/$($Name)?api-version=$($SessionVariables.apiVersion)"
                 Write-Host $uri
                 $requestParam = @{
                     Headers       = $authHeader
@@ -66,7 +82,7 @@ function Set-AzWorkspaceManager {
                 return $result
             }
             else {
-                Write-Message -FunctionName $($MyInvocation.MyCommand.Name) -Message "Error configuring Workspace Manager for workspace $($WorkspaceName)" -Severity 'Error'
+                Write-Message -FunctionName $($MyInvocation.MyCommand.Name) -Message "Error configuring Workspace Manager for workspace $($Name)" -Severity 'Error'
             }
         }
         catch {
