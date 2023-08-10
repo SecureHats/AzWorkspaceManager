6382c2b11432267993a16dedfbcbcb2dc48399ac
AzureKid <rogierdijkman@hotmail.com>
2023-08-10 18:25:42 +0200

    updated help and logic

--- src/Public/Remove-AzWorkspaceManagerAssignments.ps1
+++ src/Public/Remove-AzWorkspaceManagerAssignments.ps1
@@ -1,33 +1,22 @@
 function Remove-AzWorkspaceManagerAssignments {
-    <#
-      .SYNOPSIS
-      Remove Microsoft Sentinel Workspace Manager
-      .DESCRIPTION
-      This function removes a Microsoft Sentinel Workspace Manager Member
-      .PARAMETER WorkspaceName
-      The Name of the log analytics workspace
-      .PARAMETER ResourceGroupName
-      The name of the ResouceGroup where the log analytics workspace is located
-      .PARAMETER Name
-      The Name of the Workspace Manager Group
-      .PARAMETER Force
-      Confirms the removal of the Workspace manager configuration.
-      .EXAMPLE
-    #>
     [cmdletbinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
     param (
         [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
         [ValidatePattern('^[A-Za-z0-9][A-Za-z0-9-]+[A-Za-z0-9]$', ErrorMessage="It does not match expected pattern '{1}'")]
-        [string]$WorkspaceName,    
+        [string]$WorkspaceName,
 
         [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
         [string]$ResourceGroupName,
 
-        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $false)]
         [ValidateNotNullOrEmpty()]
         [ValidatePattern('^[A-Za-z0-9][A-Za-z0-9-]+[A-Za-z0-9]$', ErrorMessage="It does not match expected pattern '{1}'")]
         [string]$Name,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [array]$ResourceId,
 
         [Parameter(Mandatory = $false)]
         [switch]$Force
@@ -44,14 +33,29 @@ function Remove-AzWorkspaceManagerAssignments {
         else {
             $null = Get-AzWorkspaceManager -Name $WorkspaceName
         }
-        
+
         if ($Force) {
             $ConfirmPreference = 'None'
         }
 
-        try {        
-            Write-Verbose "Performing the operation 'Removing workspace manager assignment'"
-            $uri = "$($SessionVariables.workspace)/providers/Microsoft.SecurityInsights/workspaceManagerAssignments/$($Name)?api-version=$($SessionVariables.apiVersion)"
+        Write-Debug "WorkspaceName: $($WorkspaceName)"
+        Write-Debug "ResourceGroupName: $($ResourceGroupName)"
+        Write-Debug "Name: $($Name)"
+        Write-Debug "ResourceId: $($ResourceId)"
+
+        try {
+            if ($ResourceId) {
+                $uri = "https://management.azure.com$($ResourceId)?api-version=$($SessionVariables.apiVersion)"
+                $Name = $ResourceId.Split('/')[-1]
+            }
+            elseif ($Name) {
+                $uri = "$($SessionVariables.workspace)/providers/Microsoft.SecurityInsights/workspaceManagerAssignments/$($Name)?api-version=$($SessionVariables.apiVersion)"
+            }
+            else {
+                Write-Message -FunctionName $($MyInvocation.MyCommand.Name) -Message "No name for the workspace manager assignment was provided" -Severity 'Error'
+            }
+            Write-Debug "Performing the operation 'Removing workspace manager assignment'"
+            Write-Debug "Request URI: $($uri)"
 
             $requestParam = @{
                 Headers       = $authHeader
@@ -63,7 +67,7 @@ function Remove-AzWorkspaceManagerAssignments {
             $apiResponse = Invoke-RestMethod @requestParam
 
             if ($apiResponse -ne '') {
-                if ($PSCmdlet.ShouldProcess($SessionVariables.workspaceManagerConfiguration -eq 'Enabled', "Remove '$($Name)")) {    
+                if ($PSCmdlet.ShouldProcess($SessionVariables.workspaceManagerConfiguration -eq 'Enabled', "Remove Workspace Manager Assignment '$Name'")) {
                     $requestParam = @{
                         Headers       = $authHeader
                         Uri           = $uri
@@ -71,7 +75,10 @@ function Remove-AzWorkspaceManagerAssignments {
                         ErrorVariable = 'ErrVar'
                     }
 
+                    Write-Verbose "Performing the operation 'Removing workspace manager assignment'"
+                    Write-Verbose "Request URI: $($uri)"
                     Invoke-RestMethod @requestParam
+
                     Write-Host $response
                     if ($null -eq $response) {
                         Write-Message -FunctionName $($MyInvocation.MyCommand.Name) -Message "Workspace Manager Assignment '$($Name)' was removed from workspace '$WorkspaceName'" -Severity 'Information'
@@ -87,8 +94,35 @@ function Remove-AzWorkspaceManagerAssignments {
                 Write-Message -FunctionName $MyInvocation.MyCommand.Name -Message "Workspace Manager Assignment '$($Name)' was not found under workspace '$WorkspaceName'" -Severity 'Error'
             }
             else {
-                Write-Message -FunctionName $($MyInvocation.MyCommand.Name) -Message ($return.ErrorRecord | ConvertFrom-Json).error.message -Severity 'Error'
+                Write-Message -FunctionName $($MyInvocation.MyCommand.Name) -Message ($_.Exception.ErrorRecord | ConvertFrom-Json).error.message -Severity 'Error'
             }
         }
     }
+    <#
+        .SYNOPSIS
+        Remove Microsoft Sentinel Workspace Manager Assignment
+        .DESCRIPTION
+        The Remove-AzWorkspaceManagerAssignments cmdlet removes a Workspace Manager Assignment from a Microsoft Sentinel Workspace.
+        The cmdlet will not return an error if the Workspace Manager Assignment does not exist.
+        The Assignment must first be removed from the Workspace Manager Group before the group can be removed.
+        .PARAMETER WorkspaceName
+        The Name of the log analytics workspace
+        .PARAMETER ResourceGroupName
+        The name of the ResouceGroup where the log analytics workspace is located
+        .PARAMETER Name
+        The Name of the Workspace Manager Assignment
+        .PARAMETER Force
+        Confirms the removal of the Workspace manager configuration.
+        .EXAMPLE
+        Remove-AzWorkspaceManagerAssignments -WorkspaceName 'myWorkspace' -ResourceGroupName 'myRG' -Name 'myAssignment'
+
+        This command removes the Workspace Manager Assignment 'myAssignment' from the workspace 'ContosoWorkspace' in the resource group 'myRG'.
+        .EXAMPLE
+        Get-AzWorkspaceManagerAssignments -WorkspaceName 'myWorkspace' | Remove-AzWorkspaceManagerAssignments -Force
+
+        This example removes all Workspace Manager Assignments from the workspace 'ContosoWorkspace' in the resource group 'myRG' without prompting for confirmation.
+        .LINK
+        Get-AzWorkspaceManagerAssignments
+        Remove-AzWorkspaceManagerAssignments
+    #>
 }
